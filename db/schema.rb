8b0facbcaf2516d531d8d1ac33009ef7e5c3602b
Patrick <px.patrick@gmail.com>
2015-05-31 21:58:39 +1000

    add postcode as scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531111243) do
+ActiveRecord::Schema.define(version: 20150531115722) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20150531111243) do
     t.float    "wind_direction"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "postcodes", force: :cascade do |t|
+    t.string   "code"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
