d690ebb4405924220187405e05916517dd64c762
quietsalt <zhub@student.unimelb.edu.au>
2015-06-05 15:57:29 +1000

    try to solve location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -10,16 +10,10 @@ class Location < ActiveRecord::Base
 		self.where(active: true)
 	end
 
-	# NEEDS FIXING LOL
+	# not sure if its correct and Why do we need this method?
 	# return a collection of observations given a date
 	def retrive_observation time
-		observationData = Array.new
-		data[:temperature]=Observation.time.temperature
-		data[:dew_point]=Observation.time.dew_point
-		data[:rain] = Observation.time.dew_point
-		data[:wind_speed] = Observation.time.wind_speed
-		data[:wind_direction] = Observation.time.wind_direction
-		observationData.push(data)
+		observationData = observations.where("timestamp = #{time}")
 		return observationData
 	end
 end
